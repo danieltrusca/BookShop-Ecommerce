e3fe7b81c83674dbb00b54b7be441d739893692b
Dan Trusca <truscadani@yahoo.com>
2020-09-25 11:22:42 +0300

    preparing  for deployment

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.use("/api", orderRoute);
 
 const port = process.env.PORT || 8000;
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
